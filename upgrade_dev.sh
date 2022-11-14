744d106c0865315a0a997558ae99d8800c3cec8c
Terence Alderson <terence@gmail.com>
2022-11-14 15:32:52 -0500

    added migrate to the list of dev docker containers.

--- upgrade_dev.sh
+++ upgrade_dev.sh
@@ -31,7 +31,7 @@ services:
         ports:
             - "5432:5432"
     migrate:
-        image: yieldpointadmin/gdp_migrate
+        image: yieldpointadmin/gdp_migrate_dev
         container_name: migrate
         depends_on:
             - postgres
